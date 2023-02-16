0d296cc5c10fdf7ddaccdf56284744c7f1c9160b
womoboy <reza2boyce@gmail.com>
2023-02-17 02:43:45 +0330

    create object with class from data

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+class Car {
+  constructor({name, model, color}) {
+    this.name = name;
+    this.model = model;
+    this.color = color;
+  }
 
+  getInfo() {
+    console.log(`The ${this.name} ${this.name} with ${this.model} is wonderfull!`);
+  }
+}
+const data = {
+  name: "BMW",
+  model: 2011,
+  color: 'red',
+}
+
+const bmw = new Car(data)
+
+bmw.getInfo()
+bmw.getInfo()
+bmw.getInfo()
