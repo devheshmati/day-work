896c14d974dc44f6f34fb7a150f435878c92d115
womoboy <reza2boyce@gmail.com>
2023-03-01 23:59:10 +0330

    btn click test

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-class Car {
-  constructor({name, model, color}) {
-    this.name = name;
-    this.model = model;
-    this.color = color;
-  }
-
-  getInfo() {
-    console.log(`The ${this.name} ${this.name} with ${this.model} is wonderfull!`);
-  }
+function action() {
+  console.log('hello');
 }
-const data = {
-  name: "BMW",
-  model: 2011,
-  color: 'red',
-}
-
-const bmw = new Car(data)
-
-bmw.getInfo()
-bmw.getInfo()
-bmw.getInfo()
