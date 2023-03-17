3df3407cf885eab31e4e195a4545d01352590baf
womoboy <reza2boyce@gmail.com>
2023-03-18 02:36:27 +0330

    add black screen, style and basics

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function action() {
-  console.log('hello');
+var frame = document.getElementById('frame');
+var blockSize = 25;
+var cols = 21;
+var rows = 21;
+var ctx;
+
+window.onload = function() {
+  ctx = frame.getContext('2d');
+  // resize frame
+  frame.width = cols * blockSize;
+  frame.height = rows * blockSize;
+
+  // black screen
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, frame.width, frame.height);
 }
