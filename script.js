b141c881aa2fd80f86e8c94b61ae1bf87b05a450
womoboy <reza2boyce@gmail.com>
2023-03-18 19:45:12 +0330

    snake game finished

--- script.js
+++ script.js
@@ -39,6 +39,21 @@ function update() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, frame.width, frame.height);
 
+  // snake eating food
+  if(snake.pos.x == food.pos.x && snake.pos.y == food.pos.y) {
+    snake.body.push([food.pos.x, food.pos.y]);
+    placeRandom(food);
+  }
+
+  // body follow head 
+  for(let i = snake.body.length - 1; i > 0; i--) {
+    snake.body[i] = snake.body[i-1];
+  }
+
+  if(snake.body.length) {
+    snake.body[0] = [snake.pos.x, snake.pos.y];
+  } 
+
   // render foods
   ctx.fillStyle = 'red';
   ctx.fillRect(food.pos.x, food.pos.y, blockSize, blockSize);
@@ -48,6 +63,23 @@ function update() {
   snake.pos.x += velocityX * blockSize;
   snake.pos.y += velocityY * blockSize;
   ctx.fillRect(snake.pos.x, snake.pos.y, blockSize, blockSize);
+  // render snake body
+  for(let i = 0; i < snake.body.length; i++) {
+    ctx.fillRect(snake.body[i][0], snake.body[i][1], blockSize, blockSize);
+  }
+
+  // how get lose
+  // head crash
+  if(snake.pos.x < 0 || snake.pos.y < 0 || snake.pos.x >= cols * blockSize || snake.pos.y >= rows * blockSize) {
+    gameOver('You lose, Snake head crash happend!');
+  }
+
+  // body crash 
+  for(let i = 0; i < snake.body.length; i++) {
+    if(snake.pos.x == snake.body[i][0] && snake.pos.y == snake.body[i][1]) {
+      gameOver('You lose, Snake body crash happend!')
+    }
+  }
 }
 
 function placeRandom(obj) {
@@ -70,4 +102,9 @@ function moveHandler(e) {
     velocityX = 1;
     velocityY = 0;
   }
+}
+
+function gameOver(str) {
+  alert(str);
+  return;
 }
