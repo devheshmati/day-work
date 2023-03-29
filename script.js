f6ad83580892d7580d2da046c0d2ac154ef588d3
womoboy <reza2boyce@gmail.com>
2023-03-30 01:47:10 +0330

    counter dilemma example practiced

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const text = 'ksdjkfsjkjfskjd';
-const arr = text.split('');
-const uarr = new Map(Array.from(new Set(arr)).map((x,i) => [i, x]));
-console.log(uarr);
+let x = (function() {
+  let counter = 0;
+  return function() {
+    counter += 1;
+    return counter;
+  };
+})();
+
+let y = (function() {
+  for (let i = 0; i < 10; i++) {
+    x();
+    if (i == 8) {
+      return x();
+    }
+  }
+})();
+
+console.log(y);
