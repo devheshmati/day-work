11c8bf9851554fef11e45bc6d88d893f56218d2c
womoboy <reza2boyce@gmail.com>
2023-03-29 22:46:28 +0330

    converted string to The Map object with using Set and map method

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const text = 'ABCDEFGHIJKLMN';
+const text = 'ksdjkfsjkjfskjd';
 const arr = text.split('');
-
-function shuffle(arr) { 
-  for(let i = arr.length - 1; i > 0; i--) {
-    let rand = Math.floor(Math.random() * i);
-    let randItem = arr[rand];
-    arr[rand] = arr[i];
-    arr[i] = randItem;
-  }
-  return arr;
-}
-
-console.log(shuffle(arr));
+const uarr = new Map(Array.from(new Set(arr)).map((x,i) => [i, x]));
+console.log(uarr);
