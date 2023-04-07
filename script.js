b68c0b790bdda3e94056f0b6e19bf70c4ec5567d
womoboy <reza2boyce@gmail.com>
2023-04-07 23:59:33 +0330

    test

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function formHandler(e) {
-  e.preventDefault();
-  const content = document.querySelector('#content');
-  const input = document.getElementsByTagName('input')[0];
-  if(isNaN(input.value) || input.value < 0 || input.value > 10) {
-    content.innerHTML = "Invalid value";
-    return;
-  } else {
-    content.innerHTML = input.value;
-  }
+let frame, blockSize, cols, rows, velocityX, velocityY, snake, food;
+
+window.onload = () => {
+  
 }
